73306d2f9513846c1aa5fe138fdb5aaa9e22cce5
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-04-13 15:51:32 +0900

    Create Sort Colors_sol1.cpp

---
+++ Leetcode/Sort Colors_sol1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        map<int,int> ht;
+        for(int i=0;i<nums.size();i++)  ht[nums[i]]++;
+        int idx=0;
+        for(int i=0;i<ht[0];i++)   nums[idx++]=0;
+        for(int i=0;i<ht[1];i++)   nums[idx++]=1;
+        for(int i=0;i<ht[2];i++)   nums[idx++]=2;
+    }
+};
