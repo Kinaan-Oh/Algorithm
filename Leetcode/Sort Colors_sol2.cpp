4140eac8041ac728d622ffe6dd7f86658e58ed33
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-04-17 16:42:23 +0900

    Update Sort Colors_sol2.cpp

--- Leetcode/Sort Colors_sol2.cpp
+++ Leetcode/Sort Colors_sol2.cpp
@@ -1,3 +1,6 @@
+// Time Complexity: O(N), Space Complexity: O(1)
+// 키 비교 연산으로 sol1과 비교하면 더 적지만, 메모리 접근 연산으로 비교하면 swap의 사용으로(3회) sol1보다 더 많음. 따라서, 실행시간은 sol1 보다 오래걸림.
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
