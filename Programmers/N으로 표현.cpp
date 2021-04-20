3ead05dcf9f8c53a0960262389e2903ae62ecf76
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-04-20 11:03:10 +0900

    Update N으로 표현.cpp

--- Programmers/N으로 표현.cpp
+++ Programmers/N으로 표현.cpp
@@ -1,1 +1,47 @@
+#include <string>
+#include <vector>
+#include <set>
+#include <iostream>
 
+using namespace std;
+
+set<int> s[9];  // index: 1~8
+
+void makeSet(int i, int j)
+{
+    for(auto it_i=s[i].begin();it_i!=s[i].end();it_i++)
+    {
+        for(auto it_j=s[j].begin();it_j!=s[j].end();it_j++)
+        {
+            s[i+j].insert((*it_i) + (*it_j));
+            s[i+j].insert((*it_i) - (*it_j));
+            s[i+j].insert((*it_i) * (*it_j));
+            if(*it_j)    s[i+j].insert((*it_i) / (*it_j));
+        }
+    }
+}
+
+int solution(int N, int number) {
+    int answer = -1;
+    int consecutiveN = N;
+    s[1].insert(consecutiveN);
+    for(int i=2;i<=8;i++)
+    {
+        consecutiveN = (consecutiveN*10)+N;
+        for(int j=1;j<i;j++)    
+        {
+            s[i].insert(consecutiveN);
+            makeSet(j,i-j);
+        }
+    }
+    
+    for(int i=1;i<=8;i++)   
+    {
+        if(s[i].find(number)!=s[i].end())   
+        {
+            answer=i;
+            break;
+        }
+    }
+    return answer;
+}
