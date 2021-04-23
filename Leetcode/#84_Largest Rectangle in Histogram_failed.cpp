ca44d8a25e1f36a11a02fbd150d968c972b2957e
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-04-23 11:23:15 +0900

    Create #84_Largest Rectangle in Histogram_failed.cpp

---
+++ Leetcode/#84_Largest Rectangle in Histogram_failed.cpp
@@ -0,0 +1,29 @@
+// Bruteforce: O(heights.length * max(heights)). [Time Limit Exceeded(Testcase 90)]
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        set<int> h(heights.begin(),heights.end());
+        
+        int largestRectangleArea = 0;
+        
+        for(auto it = h.begin();it!=h.end();it++)
+        {
+            int maximumLengthOfRectangle = 0;
+            int lengthOfRectangle = 0;
+            for(int i=0;i<heights.size();i++)
+            {
+                if(heights[i]<*it) {
+                    maximumLengthOfRectangle = max(maximumLengthOfRectangle, lengthOfRectangle);
+                    lengthOfRectangle = 0;
+                }
+                else {
+                    lengthOfRectangle++;
+                }
+            }
+            maximumLengthOfRectangle = max(maximumLengthOfRectangle, lengthOfRectangle);
+            largestRectangleArea = max(largestRectangleArea, (*it)*maximumLengthOfRectangle);
+        }
+        return largestRectangleArea;
+    }
+};
