f2b1f21568826e3978eb39440fd72176d22c6faf
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-04-27 15:46:58 +0900

    Create 네트워크.cpp

---
+++ Programmers/네트워크.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int numOfConnectedComponent;
+bool visited[200];  // idx: 0~199
+
+void dfs(vector<vector<int>> &computers, int numOfComputer)
+{
+    int n = computers.size();
+    for(int col=0;col<n;col++)
+    {
+        if(col==numOfComputer)  continue;
+        
+        if(computers[numOfComputer][col]==1 && !visited[col]) {
+            visited[col] = true;
+            dfs(computers,col);
+        }
+    }
+}
+
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    for(int i=0;i<n;i++)
+    {
+        if(!visited[i])
+        {
+            dfs(computers,i);
+            numOfConnectedComponent++;
+        }
+    }
+    answer = numOfConnectedComponent;
+    return answer;
+}
