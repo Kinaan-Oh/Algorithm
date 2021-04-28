ea29a449334c38f5cd331765aacbc11a995e3a1b
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-04-28 18:02:35 +0900

    Update 2018_ 자동완성_failed.cpp

--- Kakao/2018_ 자동완성_failed.cpp
+++ Kakao/2018_ 자동완성_failed.cpp
@@ -1,5 +1,4 @@
-// Brute Force. Time Complexity: O(L^2)) (search.substr(0,j)를 고려하여 시간 복잡도 구하는 것이 어려웠음. j가 외부 for문에 종속이라 N-1차 함수의 최대값을 구하는 문제로 변환되지만, 나는 
-// 풀지 못하였음. search.substr(0,j)의 최대값을 L-N으로 잡을 수 있지만, 이 경우 너무 높은 경계를 잡게되어 O(L^2)이 됨.)
+// Brute Force. Time Complexity: O(L^2)) ( 정확히는 O(L^2+(L-N)^2)) = O(L^2) )
 
 #include <string>
 #include <vector>
