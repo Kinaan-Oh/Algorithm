9c2c1fedab9a87055aac4b7bd538e737c5f7ec8b
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-04-29 17:54:23 +0900

    Create #94. Binary Tree Inorder Traversal_sol2.cpp

---
+++ Leetcode/#94. Binary Tree Inorder Traversal_sol2.cpp
@@ -0,0 +1,27 @@
+// 
+
+class Solution {
+private:
+    vector<int> visited;
+    
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        TreeNode *cur = root;
+        stack<TreeNode*> st;
+        while(!st.empty()||cur)
+        {
+            if(cur) {
+                st.push(cur);
+                cur = cur->left;
+            }
+            
+            else {
+                cur = st.top();
+                st.pop();
+                visited.push_back(cur->val);
+                cur = cur->right;
+            }
+        }
+        return visited;
+    }
+};
