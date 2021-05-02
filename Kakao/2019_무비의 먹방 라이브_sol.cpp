07fdc4a5df70c1e0729338b2e91c4cf50fdb47e5
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-05-02 15:37:53 +0900

    Update 2019_무비의 먹방 라이브_sol.cpp

--- Kakao/2019_무비의 먹방 라이브_sol.cpp
+++ Kakao/2019_무비의 먹방 라이브_sol.cpp
@@ -7,5 +7,51 @@
 // (Idea) food_times를 감소시키지 않고 문제를 해결해야만 함. 이미 정렬한 food_times를 활용한다면, food_times를 감소시킬 필요 없이 문제 해결이 가능함.
 // "정렬된 food_times에서 0이 아닌 food_time의 갯수를 O(1)으로 쉽게 파악할 수 있음!" (food_times 감소 로직의 목적은 0이 아닌 food_time만 방문하기 위함이었으므로, 감소가 더이상 필요없음.)
 // food_times의 k%(0이 아닌 food_time의 갯수)번째 를 구하면 됨: O(food_times)
-// 따라서 전체 시간복잡도를 O(food_times.lengthlg(food_times.length) + food_times.length) = O(food_times.length * lg(food_times_length)) 로 개선할 수 있음.
+// 따라서 전체 시간복잡도를 O(food_times.lengthlg(food_times.length) + food_times.length) = O(food_times.length * lg(food_times_.length)) 로 개선할 수 있음.
+// 설계를 했더라도 실제 구현은 매우 어려움.(높은 수준의 구현력을 요구)
+// 정리하면 출제의도는 "정렬 및 구현".
+// 주의사항: 효율성 2번 테스트 케이스의 경우, long long 자료형을 사용하지 않으면 rectangle이 그 값을 수용하지 못하여 fail. "자료형 활용 능력" 또한 출제 의도에 포함. 
 
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int update(vector<int> &food_times_sorted, int cur) {
+    int height_cur = food_times_sorted[cur];
+    for(;cur<food_times_sorted.size();cur++) {
+        if(food_times_sorted[cur]>height_cur)   return cur;
+    }
+    return -1;
+}
+
+int solution(vector<int> food_times, long long k) {
+    vector<int> food_times_sorted = food_times;
+    sort(food_times_sorted.begin(),food_times_sorted.end());
+    int sz = food_times.size();
+    int height_prev=0, height_cur=food_times_sorted[0];
+    int cur=0;
+    while(true) {
+        long long rectangle = ((long long)height_cur-height_prev)*(sz-cur);
+        if(k-rectangle>=0) {
+            k -= rectangle;
+        }
+        else {
+            k %= (sz-cur);
+            break;
+        }
+        
+        cur = update(food_times_sorted, cur);
+        if(cur==-1) return -1;
+        height_prev = height_cur;
+        height_cur = food_times_sorted[cur];
+    }
+    
+    for(int i=0;i<food_times.size();i++) {
+        if(food_times[i]>=height_cur) {
+            if(--k<0)   return i+1;
+        }
+    }
+}
