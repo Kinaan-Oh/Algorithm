cee33dcedc03cb311a7174d2fa6e9fc8454e7ae1
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-05-04 11:51:37 +0900

    Create 프린터.cpp

---
+++ Programmers/프린터.cpp
@@ -0,0 +1,38 @@
+// Time Complexity: O(priorities.length), Space Complexity: O(priorities.length)
+
+#include <string>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    unordered_map<int,int>  ht; // priority:cnt
+    queue<int> que; // index
+    for(int i=0;i<priorities.size();i++) {
+        ht[priorities[i]]++;
+        que.push(i);
+    }
+    while(!que.empty()) {
+        int cur = que.front();
+        int cur_priority = priorities[cur];
+        bool print = true;
+        que.pop();
+        for(int i=cur_priority+1;i<=9;i++) {
+            if(ht[i]>0) {
+                que.push(cur);
+                print=false;
+                break;
+            }
+        }
+        
+        if(print) {
+            ht[cur_priority]--;
+            answer++;
+            if(cur==location)   break;
+        }
+    }
+    return answer;
+}
