2d29df75e09579f3e4af12cc92d4328bc6633e63
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-05-11 14:38:25 +0900

    Create 입국심사.cpp

---
+++ Programmers/입국심사.cpp
@@ -0,0 +1,28 @@
+// 출제의도: "이분탐색", Time Complexity: O((n*times의 최대값)*lg(n*times의 최대값)).
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+long long solution(int n, vector<int> times) {
+    long long answer = 0;
+    long long low=1 ,high=0;
+    for(int i=0;i<times.size();i++) {
+        if(high<times[i]*n) {
+            high = times[i]*n;
+        }
+    }
+    while(low<high) {
+        long long mid = (low+high)/2;
+        long long num = 0;
+        for(int i=0;i<times.size();i++) {
+            num += (mid/times[i]);
+        }
+        if(num<n)   low = mid+1;
+        else    high = mid;
+    }
+    answer = high;
+    return answer;
+}
