03a688f8b820fc6fdd0d1a5db501697827a94435
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-05-20 13:28:02 +0900

    Create 다단계 칫솔 판매.cpp

---
+++ Programmers/다단계 칫솔 판매.cpp
@@ -0,0 +1,32 @@
+// 출제의도: "트리,해시테이블", Time Complexity: O(seller.size()*h), Space Complexity: O(n). (n: enroll.size, h:노드 깊이)
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+unordered_map<string,string>    parent;
+unordered_map<string,int>   sales_profit;
+
+void distribute_profit(string name, int sales) {
+    if(sales==0)    return;
+    int profit_distribution = sales/10;
+    sales_profit[name] += (sales-profit_distribution);
+    distribute_profit(parent[name], profit_distribution);
+}
+
+vector<int> solution(vector<string> enroll, vector<string> referral, vector<string> seller, vector<int> amount) {
+    vector<int> answer;
+    for(int i=0;i<enroll.size();i++) {
+        parent[enroll[i]] = referral[i];
+    }
+    for(int i=0;i<seller.size();i++) {
+        distribute_profit(seller[i], amount[i]*100);
+    }
+    for(int i=0;i<enroll.size();i++) {
+        answer.push_back(sales_profit[enroll[i]]);
+    }
+    return answer;
+}
