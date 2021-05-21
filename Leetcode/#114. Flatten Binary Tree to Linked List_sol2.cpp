337f922e71966c99fd5b6b8bf3742e186ac0baa4
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-05-21 10:10:30 +0900

    Create #114. Flatten Binary Tree to Linked List_sol2.cpp

---
+++ Leetcode/#114. Flatten Binary Tree to Linked List_sol2.cpp
@@ -0,0 +1,23 @@
+// 출제의도: "이진트리, 구현(반복)", Time Complexity: O(n), Space Complexity: O(1).
+// Iterative 구현방식(In-place)
+
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        while(root) {
+            TreeNode *left = root->left;
+            if(left==nullptr) {
+                root = root->right;
+                continue;
+            }
+            
+            while(left->right) {
+                left = left->right;
+            }
+            left ->right = root->right;
+            root->right = root->left;
+            root->left = nullptr;
+            root = root->right;
+        }
+    }
+};
