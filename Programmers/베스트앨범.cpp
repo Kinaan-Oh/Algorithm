f6ff3df12b550338813b6cb8342e10c5332317f1
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-05-21 15:56:21 +0900

    Update 베스트앨범.cpp

--- Programmers/베스트앨범.cpp
+++ Programmers/베스트앨범.cpp
@@ -8,19 +8,19 @@
 
 using namespace std;
 
-unordered_map<string,int>  ht; // [genre,count]
+unordered_map<string,int>  sum_of_genre; // [genre,count]
 unordered_map<string,int> num_of_genre; // [genre,count]
 vector<string>  genres_copied;
 vector<int> plays_copied;
 
 bool compare(int id1, int id2) {
-    if(ht[genres_copied[id1]]==ht[genres_copied[id2]]) {
+    if(sum_of_genre[genres_copied[id1]]==sum_of_genre[genres_copied[id2]]) {
         if(plays_copied[id1]==plays_copied[id2]) {
             return id1<id2;
         }
         return plays_copied[id1]>plays_copied[id2];
     }
-    return ht[genres_copied[id1]]>ht[genres_copied[id2]];
+    return sum_of_genre[genres_copied[id1]]>sum_of_genre[genres_copied[id2]];
 }
 
 vector<int> solution(vector<string> genres, vector<int> plays) {
@@ -29,7 +29,7 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
     genres_copied = genres;
     plays_copied = plays;
     for(int i=0;i<genres.size();i++) {
-        ht[genres[i]] += plays[i];
+        sum_of_genre[genres[i]] += plays[i];
     }
     for(int i=0;i<genres.size();i++)    id.push_back(i);
     sort(id.begin(),id.end(),compare);
