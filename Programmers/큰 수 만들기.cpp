8430ffea5826245fc9d45f5ac2de457619f48e99
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-05-24 20:17:25 +0900

    Create 큰 수 만들기.cpp

---
+++ Programmers/큰 수 만들기.cpp
@@ -0,0 +1,34 @@
+//  출제의도: "greedy", Time Complexity: O(n^2), Space Complexity: O(1). (n: number.size)
+//  number[i]<number[i+1] 인 가장 앞인 i를 제거. 만약, 내림차순 정렬되어있다면 가장 뒤의 원소를 제거.
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void erase(string &number) {
+    if(number.size()==1) {
+        number = "";
+        return;
+    }
+    
+    for(int i=0;i<number.size()-1;i++) {
+        if(number[i]<number[i+1]) {
+            number.erase(number.begin()+i);
+            break;
+        }
+        
+        if(i==number.size()-2) {
+            number.erase(--number.end());
+        }
+    }
+}
+
+string solution(string number, int k) {
+    string answer = "";
+    for(int i=0;i<k;i++) {
+        erase(number);
+    }
+    answer = number;
+    return answer;
+}
