ff10781f66cfe5c15ee0210cae130ccca88ee25e
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-05-26 15:42:03 +0900

    Create 구명보트_failed.cpp

---
+++ Programmers/구명보트_failed.cpp
@@ -0,0 +1,47 @@
+// 출제의도 : "greedy", Time Complexity: O(n^3), Space Complexity: O(1).
+
+#include <string>
+#include <vector>
+#include <climits>
+#include <iostream>
+
+using namespace std;
+
+bool get_people_aboard(vector<int> &people, int limit) {
+    bool if_people_in_island = false;
+    int max_boat_weight = INT_MIN;
+    vector<int> passenger;
+    
+    for(int i=0;i<people.size();i++) {
+        if(people[i]>limit)   continue;
+        
+        if_people_in_island = true;
+        int boat_weight = people[i];
+        if(boat_weight>max_boat_weight) {
+            max_boat_weight = boat_weight;
+            passenger = {i};
+        }
+            
+        for(int j=i+1;j<people.size();j++) {
+            if(boat_weight+people[j]>limit) continue;
+            
+            if(boat_weight+people[j]>max_boat_weight) {
+                max_boat_weight = boat_weight+people[j];
+                passenger = {i,j};
+            }
+        }
+    }
+    
+    for(int i=0;i<passenger.size();i++) {
+        people[passenger[i]] = limit+1;
+    }
+    return if_people_in_island;
+}
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    while(get_people_aboard(people,limit)) {
+        answer++;
+    }
+    return answer;
+}
