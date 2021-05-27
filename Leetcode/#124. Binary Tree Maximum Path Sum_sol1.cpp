62a0daca7bbe23058b531ab9c06b8b8fa621e51c
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-05-27 13:26:03 +0900

    Update #124. Binary Tree Maximum Path Sum_sol1.cpp

--- Leetcode/#124. Binary Tree Maximum Path Sum_sol1.cpp
+++ Leetcode/#124. Binary Tree Maximum Path Sum_sol1.cpp
@@ -1,6 +1,6 @@
 // 출제의도: "이진트리, 재귀", Time Complexity: O(2n) = O(n), Space Complexity: O(n). (n: 트리 노드의 갯수)
 // 점화식: 어떤 트리의 maximum path sum = max(root를 포함하는 max path sum, leftsubtree의 maximum path sum(root 포함X), rightsubtree의 maximum path sum(root 포함X)).
-// maxPathSum에서 maxDistToLeaf의 중복호출을 막이 위해 메모이제이션(dp) 사용. 주어지는 트리정보로 각 노드를 식별하기 어려워서 TreeNode*를 키(key)로한 해시테이블을 구성. 
+// maxPathSum에서 maxDistToLeaf의 중복호출을 막이 위해 메모이제이션(dp) 사용. 주어지는 트리정보로 각 노드를 식별하기 어렵기때문에 TreeNode*를 키(key)로 한 해시테이블을 구성. 
 
 class Solution {
 private:
