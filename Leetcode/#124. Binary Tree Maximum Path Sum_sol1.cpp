f80ec8f44006785063ff7e308c1290318e0a9dad
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-05-27 12:27:39 +0900

    Create #124. Binary Tree Maximum Path Sum_sol1.cpp

---
+++ Leetcode/#124. Binary Tree Maximum Path Sum_sol1.cpp
@@ -0,0 +1,27 @@
+// 출제의도: "이진트리, 재귀", Time Complexity: O(n), Space Complexity: O(n). (n: 트리 노드의 갯수)
+// 점화식: 어떤 트리의 maximum path sum = max(root를 포함하는 path, leftsubtree의 maximum path sum, rightsubtree의 maximum path sum).
+// maxPathSum에서 maxDistToLeaf의 중복호출을 막이 위해 메모이제이션(dp) 사용. 주어지는 트리정보로 노드의 아이덴터티를 식별하기 어려워서(예를들면 노드 id), TreeNode*를 키로한 해시테이블을 구성. 
+
+class Solution {
+private:
+    unordered_map<TreeNode*,int>    dp;    
+
+public:
+    int maxPathSum(TreeNode* root) {
+        if(root==nullptr)   return INT_MIN;
+        
+        int pathIncludingRoot = root->val, distLeft = maxDistToLeaf(root->left), distRight = maxDistToLeaf(root->right);
+        if(distLeft>=0)     pathIncludingRoot += distLeft;
+        if(distRight>=0)    pathIncludingRoot += distRight;
+        
+        return max(max(maxPathSum(root->left), maxPathSum(root->right)), pathIncludingRoot);
+    }
+    
+    int maxDistToLeaf(TreeNode* root) {
+        if(root==nullptr)   return 0;
+        if(dp[root])    return dp[root];
+        int maxDist = root->val, distLeft = maxDistToLeaf(root->left), distRight = maxDistToLeaf(root->right);
+        if(max(distLeft,distRight)>=0)  maxDist += max(distLeft,distRight);
+        return dp[root] = maxDist;
+    }
+};
