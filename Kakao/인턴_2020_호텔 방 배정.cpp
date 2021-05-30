6b1526d593febfd759790983cbb4e1542661c767
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-05-30 20:20:44 +0900

    Update 인턴_2020_호텔 방 배정.cpp

--- Kakao/인턴_2020_호텔 방 배정.cpp
+++ Kakao/인턴_2020_호텔 방 배정.cpp
@@ -1,6 +1,6 @@
 // 출제의도: "Union-Find", Time Complexity: O(nlg*n), Space Complexity: O(n). (n: room_number.size)
 // 할당한 방은 다음에 할당할 방의 번호를 가리킨다. 다음에 할당할 방을 할당했을 때, 해당 방을 가리키던 방들의 정보를 해시테이블에서 업데이트 할 수 없음. 따라서, Find(Path Compression)로 재귀적으로 찾아야 함.
-// Path Compression으로 구현하여, Find의 n회 반복은 Time Complexity(nlg*n)가 됨.
+// Path Compression으로 구현하여, Find의 n회 반복은 Time Complexity: O(nlg*n)가 됨. 이는 Brute-force하게 구현할 때의 복잡도 O(n^2)보다 훨씬 효율적인 코드.
 
 #include <string>
 #include <vector>
