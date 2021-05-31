3727f41bd0ecba3ba229497af25c85da1b2ae846
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-05-31 10:59:38 +0900

    Update and rename 인턴_2019_징검다리 건너기.cpp to 인턴_2019_징검다리 건너기_sol1.cpp

--- Kakao/인턴_2019_징검다리 건너기_sol1.cpp
+++ Kakao/인턴_2019_징검다리 건너기_sol1.cpp
@@ -1,4 +1,4 @@
-// 출제의도: "이분탐색, ", Time Complexity: O(nlgn), Space Complexity: O(n). (n: stones.size)
+// 출제의도: "이분탐색 or 슬라이딩 윈도우 ", Time Complexity: O(nlgn), Space Complexity: O(n). (n: stones.size)
 // 1개씩 제거하지 않고 stone 크기만큼 제거하는 아이디어는 "무지의 먹방 라이브" 문제와 동일.
 // 처음에는 stones의 크기가 작은 것에서 큰 순서대로 is_possible을 호출하여 구현하였으나 Time Complexity가 O(n^2)로 효율성을 통과하지 못함.
 // is_possible이 false을 리턴하는 최초의 stone을 찾기 위해 "이분 탐색"을 사용하면 O(nlgn)으로 개선할 수 있음. 
