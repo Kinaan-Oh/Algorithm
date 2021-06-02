fb2c9c72a76df27b4b44f208175871274ba132e2
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-06-02 14:31:54 +0900

    Create #136. Single Number_sol3.cpp

---
+++ Leetcode/#136. Single Number_sol3.cpp
@@ -0,0 +1,14 @@
+// "Bitmask(XOR)", Time Complexity: O(n), Space Complexity: O(1).
+// XOR 연산자 : 1)교환법칙, 2) a xor 0 == a, 3) a xor a == 0.
+// (a xor b) xor a == (a xor a) xor b = 0 xor b = b
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int memo=0;
+        for(int i=0;i<nums.size();i++) {
+            memo ^= nums[i];
+        }
+        return memo;
+    }
+};
