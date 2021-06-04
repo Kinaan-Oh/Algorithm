e2a533a419398a9cdfe27adc37ec41c839fd532d
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-06-04 16:33:15 +0900

    Update #138. Copy List with Random Pointer_sol2.cpp

--- Leetcode/#138. Copy List with Random Pointer_sol2.cpp
+++ Leetcode/#138. Copy List with Random Pointer_sol2.cpp
@@ -1,4 +1,4 @@
-// 출제의도: "리스트, 해시테이블, 그래프 순회(dfs)", Time Complexity: O(n), Space Complexity: O(n). (n: list.length)
+// 출제의도: "리스트, 해시테이블, 재귀(그래프 dfs)", Time Complexity: O(n), Space Complexity: O(n). (n: list.length)
 // 핵심 아이디어: list를 graph로 간주할 수 있다.
 // dfs로 기존 list를 순회하면서, next와 random의 노드가 없다면 생성하여 붙여주고, 이미 생성했다면 붙여주기만 한다. 이미 copied되었는지 파악하기 위해 해시 테이블을 사용한다.
 
