6e9c641dde674a63a0b67dd9ea82b6b0b742bb8a
Kinaan91 <81892038+Kinaan91@users.noreply.github.com>
2021-06-12 23:57:00 +0900

    Create 2019_후보키.swift

---
+++ Kakao/2019_후보키.swift
@@ -0,0 +1,32 @@
+// 출제의도: "Bitmask"
+// (1) HashSet -> 중복성 검사 (2) BitMask -> 유일성 검사
+// "a&b == a" or "a|b == b" : a가 b에 포함된다.
+
+import Foundation
+
+func solution(_ relation:[[String]]) -> Int {
+    let rowSize = relation.count
+    let colSize = relation[0].count
+    var candidateKeys = Set<Int>()
+
+    for select in 1..<(1<<colSize) {
+        var relationSelected = Set<String>()
+
+        for i in 0..<rowSize {
+            var tuple = ""
+            for j in 0..<colSize where (select & (1<<(colSize-1-j))) != 0 {
+                if j>0 {
+                    tuple += " "
+                }
+                tuple += relation[i][j]
+            }
+            relationSelected.insert(tuple)
+        }
+
+        if relationSelected.count==relation.count && candidateKeys.filter({(select|$0)==select}).isEmpty {
+            candidateKeys.insert(select)
+        }
+    }
+    
+    return candidateKeys.count
+}
