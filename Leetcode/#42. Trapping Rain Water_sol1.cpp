a4e795a3214a7bd629695e139771f476bc6c25dc
Kinaan-Oh <81892038+Kinaan-Oh@users.noreply.github.com>
2021-06-15 14:57:13 +0900

    Update #42. Trapping Rain Water_sol1.cpp

--- Leetcode/#42. Trapping Rain Water_sol1.cpp
+++ Leetcode/#42. Trapping Rain Water_sol1.cpp
@@ -1,4 +1,5 @@
-// "Brute Force", Time Complexity: O(n), Space Complexity: O(1). (n: height.size)
+// NHN 코테 2번 문제와 동일. 현장에서 이 방법으로 풀이함.
+// "Two-pointer", Time Complexity: O(n), Space Complexity: O(1). (n: height.size)
 // idea: 직전 높이와 같거나 큰 높이를 만나게 될 때까지 빗물은 고인다.
 // 왼쪽에서 오른쪽으로, 오른쪽에서 왼쪽으로 2번 탐색.
 // 2 0 2 케이스를 처리하기 위해 높이가 같은 경우는 2번의 탐색 중 하나만 처리하도록 해야 함.
