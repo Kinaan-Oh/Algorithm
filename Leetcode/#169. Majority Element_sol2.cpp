ac93f9887509f33ed89022669c3416b0403e1b67
Kinaan-Oh <81892038+Kinaan-Oh@users.noreply.github.com>
2021-07-07 18:54:01 +0900

    Update #169. Majority Element_sol2.cpp

--- Leetcode/#169. Majority Element_sol2.cpp
+++ Leetcode/#169. Majority Element_sol2.cpp
@@ -3,16 +3,25 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int candidate;
-        int sum = 0;
-        for(int i=0;i<nums.size();i++) {
-            if(sum==0) {
-                candidate = nums[i];
-            }
-            
-            sum += (nums[i]==candidate ? 1 : -1);
-        }
+        return majorityElementInPart(nums, 0, nums.size()-1);
+    }
+    
+    int majorityElementInPart(vector<int>& nums, int begin, int end) {
+        if(begin==end)  return nums[begin];
         
-        return candidate;
+        int mid = (begin+end)/2;
+        int left = majorityElementInPart(nums, begin, mid);
+        int right = majorityElementInPart(nums, mid+1, end);
+        
+        if(left == right)   return left;
+        return count(nums, begin, end, left)<count(nums, begin, end, right) ? right : left;
+    }
+    
+    int count(vector<int>& nums, int begin, int end, int num) {
+        int cnt = 0;
+        for(int i=begin;i<=end;i++) {
+            if(nums[i]==num)    cnt++;
+        }
+        return cnt;
     }
 };
