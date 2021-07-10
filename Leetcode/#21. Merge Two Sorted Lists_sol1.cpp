2133b00acfde77cfc697ce5d72d98c2e27599aea
Kinaan-Oh <81892038+Kinaan-Oh@users.noreply.github.com>
2021-07-10 20:51:39 +0900

    Update and rename 21. Merge Two Sorted Lists_sol1.cpp to #21. Merge Two Sorted Lists_sol1.cpp

--- Leetcode/#21. Merge Two Sorted Lists_sol1.cpp
+++ Leetcode/#21. Merge Two Sorted Lists_sol1.cpp
@@ -19,17 +19,8 @@ public:
             }
         }
         
-        while(l1) {
-            cur->next = l1;
-            cur = cur->next;
-            l1 = l1->next;
-        }
-        
-        while(l2) {
-            cur->next = l2;
-            cur = cur->next;
-            l2 = l2->next;
-        }
+        if(l1)  cur->next = l1;
+        if(l2)  cur->next = l2;
         
         return head->next;
     }
