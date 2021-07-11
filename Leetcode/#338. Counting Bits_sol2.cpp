e23fa09543b243342f3446b8221eff7f2d425fba
Kinaan-Oh <81892038+Kinaan-Oh@users.noreply.github.com>
2021-07-11 13:54:14 +0900

    Update #338. Counting Bits_sol2.cpp

--- Leetcode/#338. Counting Bits_sol2.cpp
+++ Leetcode/#338. Counting Bits_sol2.cpp
@@ -3,23 +3,19 @@
 // 점화식: memo[n] = memo[n-cur]+1(cur: most significant bit).
 
 class Solution {
-private:
-    int memo[100000];
 public:
     vector<int> countBits(int n) {
         if(n==0)    return {0};
         
-        fill(memo,memo+100000,0);
+        vector<int> memo(n+1,0);
         memo[0] = 0, memo[1] = 1;
-        vector<int> ans = {0, 1};
         int cur = (1<<1);
         
         for(int i=2;i<=n;i++) {
             cur = i&(cur<<1) ? cur<<1 : cur;
             memo[i] = memo[i-cur]+1;
-            ans.push_back(memo[i]);
         }
         
-        return ans;
+        return memo;
     }
 };
