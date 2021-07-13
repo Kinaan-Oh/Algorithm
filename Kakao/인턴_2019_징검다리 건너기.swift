b41fb37b3e23af29fe243c2112ffe79fc7590cc2
Kinaan-Oh <81892038+Kinaan-Oh@users.noreply.github.com>
2021-07-13 23:07:54 +0900

    Update 인턴_2019_징검다리 건너기.swift

--- Kakao/인턴_2019_징검다리 건너기.swift
+++ Kakao/인턴_2019_징검다리 건너기.swift
@@ -1,6 +1,6 @@
 // leetcode #239. Sliding Window Maximum과 같은 문제.
 // 출제의도: "Sliding Window",
-// 풀이 방법: "Monotone Queue", Time Complexity: O(n), Space Complexity: O(n). (n: stones.size)
+// 풀이 방법: "Monotone Queue", Time Complexity: O(n), Space Complexity: O(k). (n: stones.size)
 // 다양한 풀이 방법이 있지만(이분탐색, 슬라이딩 윈도우(bst), union-find, monotone queue) 가장 효율성 높은 방법은 monotone queue.
 // 문제를 해석하고 Sliding Window Maximum 를 구하는 문제로 번역 하는 과정이 필요. 해당 문제를 미리 풀어보지 않았다면 Brute Force에 까까운 이분탐색 외에 절대 스스로 다른 풀이를 생각할 수 없음.
 
