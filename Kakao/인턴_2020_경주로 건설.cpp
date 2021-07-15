220aeba8669666748b5455828fa1e2178f2ee183
Kinaan-Oh <81892038+Kinaan-Oh@users.noreply.github.com>
2021-07-15 15:11:04 +0900

    Update 인턴_2020_경주로 건설.cpp

--- Kakao/인턴_2020_경주로 건설.cpp
+++ Kakao/인턴_2020_경주로 건설.cpp
@@ -4,66 +4,144 @@
 // hop(홉)이 더 많다고 해서 비용이 반드시 비싸다고 할 수는 없게 됨. 따라서, 해당 지점에 대해 방문 비용을 저장해놓고, 나중에 더 저렴한 비용으로 방문(충분히 가능하다)하게 되면 업데이트하고 탐색을 진행함.
 // 이 문제에서 주의사항은, 초기위치(0,0)에서 이동할 경우 코너는 형성되지 않는다는 것을 고려해주어야함.
 
+// [7/15] 코드 개선
 #include <string>
 #include <vector>
-#include <queue>
-#include <algorithm>
 #include <climits>
+#include <queue>
 #include <iostream>
 
 using namespace std;
 
-typedef pair<int,int>   Pos;
+vector<vector<int>>    board;
+int dx[4] = {0, 1, 0, -1}, dy[4] = {1, 0, -1, 0}; // 0:east, 1:south, 2:west, 3:north
+int cost[25][25][4];    // [x][y][dir]
+
+struct Pos {
+    int x, y;
+    
+    Pos(int X,int Y): x(X),y(Y) {}
+    
+    Pos operator+(Pos target) {
+        return Pos(x+target.x, y+target.y);
+    }
+    
+    bool operator==(Pos target) {
+        return x==target.x && y==target.y;
+    }
+    
+    bool is_out_of_bound() {
+        int N = board.size();
+        if(x<0 || x>=N || y<0 || y>=N)  return true;
+        return false;
+    }
+};
 
 struct Car {
     Pos pos;
-    int dir;
     int cost;
-    Car(Pos p,int D,int C=0):pos(p),dir(D),cost(C){}
+    int dir;    // 0:east, 1:south, 2:west, 3:north     
+    
+    Car(Pos p, int c,int d): pos(p),cost(c),dir(d) {}  
 };
 
-int dx[4] = {1,0,-1,0};     // dir: South East North West
-int dy[4] = {0,1,0,-1};     
-int cost[25][25][4];    // [x][y][dir]
-int answer = INT_MAX;
-
-bool is_out_of_bound(int sz, Pos pos) {
-    if(pos.first<0 || pos.first>=sz || pos.second<0 || pos.second>=sz)  return true;
-    return false;
-}
-
-void bfs(vector<vector<int>> &board) {
-    int sz_of_board = board.size();
-    Pos dest = Pos(sz_of_board-1,sz_of_board-1);
+int bfs() {
+    int N = board.size();
+    Pos dest = Pos(N-1,N-1);
     queue<Car>  que;
-    cost[0][0][0] = cost[0][0][1] = 0;
-    if(!board[1][0]) {
-        cost[1][0][0] = 100;
-        que.push(Car(Pos(1,0),0,100));
-    }
-    if(!board[0][1]) {
-        cost[0][1][1] = 100;
-        que.push(Car(Pos(0,1),1,100));
-    }
+    cost[0][0][0] = 0, cost[0][0][1] = 0, cost[0][0][2] = 0, cost[0][0][3] = 0;
+    que.push(Car(Pos(0,0),0,0));
     
     while(!que.empty()) {
-        Car car_cur = que.front();
-        que.pop();
-        if(car_cur.pos == dest)  answer = min(answer, car_cur.cost);
+        Car cur = que.front();
         
-        for(int i=0;i<4;i++) {
-            Pos pos_next = Pos(car_cur.pos.first+dx[i], car_cur.pos.second+dy[i]);
-            int cost_next = car_cur.dir!=i ? car_cur.cost+600 : car_cur.cost+100;
-            if(is_out_of_bound(sz_of_board,pos_next) || cost[pos_next.first][pos_next.second][i]<=cost_next || board[pos_next.first][pos_next.second]) continue;
+        que.pop();
+        if(cur.pos == dest)    continue;
+        
+        for(int dir=0;dir<4;dir++) {
+            Pos next_pos = cur.pos + Pos(dx[dir],dy[dir]);
+            if(next_pos.is_out_of_bound() || board[next_pos.x][next_pos.y])    continue;
             
-            cost[pos_next.first][pos_next.second][i] = cost_next;
-            que.push(Car(pos_next,i,cost_next));
+            int cost_corner = (abs(cur.dir-dir)==1 || abs(cur.dir-dir)==3) && !(cur.pos==Pos(0,0)) ? 500 : 0;
+            int next_cost = cur.cost + 100 + cost_corner;
+
+            if(next_cost>=cost[next_pos.x][next_pos.y][dir])    continue;
+            
+            cost[next_pos.x][next_pos.y][dir] = next_cost;
+            que.push(Car(next_pos, next_cost, dir));
         }
     }
+    
+    return min(cost[N-1][N-1][0], cost[N-1][N-1][1]);
 }
 
-int solution(vector<vector<int>> board) {
-    fill(&cost[0][0][0],&cost[0][0][0]+25*25*4,INT_MAX);
-    bfs(board);
+int solution(vector<vector<int>> param0) {
+    board = param0;
+    fill(&cost[0][0][0], &cost[0][0][0]+25*25*4, INT_MAX);
+    int answer = bfs();
     return answer;
 }
+
+// #include <string>
+// #include <vector>
+// #include <queue>
+// #include <algorithm>
+// #include <climits>
+// #include <iostream>
+
+// using namespace std;
+
+// typedef pair<int,int>   Pos;
+
+// struct Car {
+//     Pos pos;
+//     int dir;
+//     int cost;
+//     Car(Pos p,int D,int C=0):pos(p),dir(D),cost(C){}
+// };
+
+// int dx[4] = {1,0,-1,0};     // dir: South East North West
+// int dy[4] = {0,1,0,-1};     
+// int cost[25][25][4];    // [x][y][dir]
+// int answer = INT_MAX;
+
+// bool is_out_of_bound(int sz, Pos pos) {
+//     if(pos.first<0 || pos.first>=sz || pos.second<0 || pos.second>=sz)  return true;
+//     return false;
+// }
+
+// void bfs(vector<vector<int>> &board) {
+//     int sz_of_board = board.size();
+//     Pos dest = Pos(sz_of_board-1,sz_of_board-1);
+//     queue<Car>  que;
+//     cost[0][0][0] = cost[0][0][1] = 0;
+//     if(!board[1][0]) {
+//         cost[1][0][0] = 100;
+//         que.push(Car(Pos(1,0),0,100));
+//     }
+//     if(!board[0][1]) {
+//         cost[0][1][1] = 100;
+//         que.push(Car(Pos(0,1),1,100));
+//     }
+    
+//     while(!que.empty()) {
+//         Car car_cur = que.front();
+//         que.pop();
+//         if(car_cur.pos == dest)  answer = min(answer, car_cur.cost);
+        
+//         for(int i=0;i<4;i++) {
+//             Pos pos_next = Pos(car_cur.pos.first+dx[i], car_cur.pos.second+dy[i]);
+//             int cost_next = car_cur.dir!=i ? car_cur.cost+600 : car_cur.cost+100;
+//             if(is_out_of_bound(sz_of_board,pos_next) || cost[pos_next.first][pos_next.second][i]<=cost_next || board[pos_next.first][pos_next.second]) continue;
+            
+//             cost[pos_next.first][pos_next.second][i] = cost_next;
+//             que.push(Car(pos_next,i,cost_next));
+//         }
+//     }
+// }
+
+// int solution(vector<vector<int>> board) {
+//     fill(&cost[0][0][0],&cost[0][0][0]+25*25*4,INT_MAX);
+//     bfs(board);
+//     return answer;
+// }
