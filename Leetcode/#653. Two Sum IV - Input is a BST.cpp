060e201e9df522eec0069ba440647e6568e82c28
Kinaan-Oh <81892038+Kinaan-Oh@users.noreply.github.com>
2021-07-25 02:17:03 +0900

    Create #653. Two Sum IV - Input is a BST.cpp

---
+++ Leetcode/#653. Two Sum IV - Input is a BST.cpp
@@ -0,0 +1,27 @@
+// "BST-Inorder Traverse, Two-Pointer", Time Complexity: O(n), Space Complexity: O(n). (n: root 노드 갯수)
+
+class Solution {
+private:
+    vector<int> val;
+public:
+    bool findTarget(TreeNode* root, int k) {
+        LVR(root);
+        return twoSum(k);
+    }
+    
+    void LVR(TreeNode* root) {
+        if(root->left)    LVR(root->left);
+        val.push_back(root->val);
+        if(root->right)    LVR(root->right);
+    }
+    
+    bool twoSum(int k) {
+        int left = 0, right = val.size()-1;
+        while(left<right) {
+            if(val[left]+val[right] == k)   return true;
+            else if(val[left]+val[right] < k)   left++;
+            else    right--;
+        }
+        return false;
+    }
+};
