657446d7c717656d87de970e778ec9d64525511b
Kinaan-Oh <81892038+Kinaan-Oh@users.noreply.github.com>
2021-12-11 16:22:37 +0900

    Create #22. Generate Parentheses_sol2.cpp

---
+++ Leetcode/#22. Generate Parentheses_sol2.cpp
@@ -0,0 +1,30 @@
+// BackTrack, Time Complexity: O(4^n/n^3/2), Space Complexity: O(n).
+// 전형적인 Catalan Number 문제.
+// 점화식: Cn = C0*Cn-1 + C1*Cn-2 + .. + Cn-2*C1 + Cn-1*C0.
+// Stirling’s approximation에 의해 Time Complexity는 4^n/n^3/2 로 근사된다.
+
+class Solution {
+private:
+    vector<string> answer;
+    
+public:
+    vector<string> generateParenthesis(int n) {
+        backtrack("", n, 0, 0);
+        
+        return answer;
+    }
+    
+    void backtrack(string parenthesis, int n, int left, int right) {
+        if (parenthesis.length() == 2*n) {
+            answer.push_back(parenthesis);
+            return;
+        }
+        
+        if (left<n) {
+            backtrack(parenthesis+"(", n, left+1, right);
+        }
+        if (right<n && left>right) {
+            backtrack(parenthesis+")", n, left, right+1);
+        }
+    }
+};
