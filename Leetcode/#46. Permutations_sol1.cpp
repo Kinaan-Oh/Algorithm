1cfdf48eddd500a7e37725166b217e6ad32e4564
Kinaan-Oh <81892038+Kinaan-Oh@users.noreply.github.com>
2021-12-13 15:20:58 +0900

    Update #46. Permutations_sol1.cpp

--- Leetcode/#46. Permutations_sol1.cpp
+++ Leetcode/#46. Permutations_sol1.cpp
@@ -1,4 +1,4 @@
-// 출제의도: backtrack, Time Complexity: O(), Space Complexity: O().
+// 출제의도: backtrack
 // next_permutation은 의미가 없기에, 단순한 풀이인 dfs 선택.
 // dfs를 좀더 효율적으로 개선가능할 것으로 생각됨.
 // next_permutation을 구현해 본적이 있기 때문에, 해당 구현 원리를 이용한 풀이 역시 가능할 것으로 생각됨.
