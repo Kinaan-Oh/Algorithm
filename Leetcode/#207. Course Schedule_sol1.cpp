73a47476d85b94316034172e3ec9d66bd8861fd7
Kinaan-Oh <81892038+Kinaan-Oh@users.noreply.github.com>
2021-12-14 13:57:21 +0900

    Update and rename #207. Course Schedule_sol1.cpp to Leetcode/#207. Course Schedule_sol1.cpp

--- Leetcode/#207. Course Schedule_sol1.cpp
+++ Leetcode/#207. Course Schedule_sol1.cpp
@@ -1,5 +1,6 @@
 // 출제의도: Topological Sort, Time Complexity: O(V+E), Space Complexity: O(V+E).
 // 전형적인 Topological Sort 유형.
+// 논리적 오류는 cycle로 귀결된다. Topological Sort는 DAG 인 경우만 가능하다.
 
 
 class Solution {
