1533c9c6e5b634d5e314c16495dfd1d6e9ece2dd
Kinaan-Oh <81892038+Kinaan-Oh@users.noreply.github.com>
2021-12-27 21:59:34 +0900

    Create #543. Diameter of Binary Tree_sol2.cpp

---
+++ Leetcode/#543. Diameter of Binary Tree_sol2.cpp
@@ -0,0 +1,27 @@
+// 4ms으로 sol1과 동등한 성능. 
+// 점화식 : diameter = Max of 1) diameter of left subtree
+//                          2) diameter of right subtree
+//                          3) depth of left subtree + depth of right subtree
+
+class Solution {
+private:
+    unordered_map<TreeNode*,int>    depth;
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        maxDepth(root);
+        return diameter(root);
+    }
+    
+    int diameter(TreeNode* root) {
+        if(root == nullptr) return 0;
+        
+        return max(max(diameter(root->left), diameter(root->right)), depth[root->left]+depth[root->right]);
+    }
+    
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr) return 0;
+        
+        depth[root] = max(maxDepth(root->left), maxDepth(root->right)) + 1;
+        return depth[root];
+    }
+};
