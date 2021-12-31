9266f27046ce67cd18a140ec5d0f6169d425f902
Kinaan-Oh <81892038+Kinaan-Oh@users.noreply.github.com>
2021-12-31 22:33:19 +0900

    Update #208. Implement Trie (Prefix Tree).cpp

--- Leetcode/#208. Implement Trie (Prefix Tree).cpp
+++ Leetcode/#208. Implement Trie (Prefix Tree).cpp
@@ -4,48 +4,48 @@
 // Trie 자료구조는 "1) 같은 prefix를 같은 모든 문자열의 검색 2) 사전순 문자열 나열 3) 적은 공간으로 많은 문자열을 저장" 등의 이점이 있다
 // 길이가 m인 문자열 탐색의 경우 BST가 O(mlgn)이 걸리는 반면, 트라이는 O(m)으로 우월하다.(n: BST 원소의 갯수) 
 
+// 12/31 Refactor: 좀 더 간결하게 변경
+
 class Trie {
 private:
-    unordered_map<char,Trie*>   child;
-    int num_of_tail;
-    bool tail;
+    unordered_map<char, Trie*>  children;
+    bool is_tail;
+    
 public:
-    /** Initialize your data structure here. */
     Trie() {
-        this->num_of_tail = 0;
-        this->tail = false;
+        is_tail = false;
     }
     
-    /** Inserts a word into the trie. */
     void insert(string word) {
-        Trie* cur = this;
-        cur->num_of_tail += 1;
+        Trie* next = this;
         
-        for(int i=0;i<word.length();i++) {
-            if(cur->child[word[i]]==nullptr)    cur->child[word[i]] = new Trie();
-            cur = cur->child[word[i]];
-            cur->num_of_tail += 1;
+        for(auto c: word) {
+            if(next->children[c] == nullptr)    next->children[c] = new Trie();
+            
+            next = next->children[c];
         }
-        cur->tail = true;
+        next->is_tail = true;
     }
     
-    /** Returns if the word is in the trie. */
     bool search(string word) {
-        Trie* cur = this;
-        for(int i=0;i<word.length();i++) {
-            if(cur->child[word[i]]==nullptr)    return false;
-            cur = cur->child[word[i]];
+        Trie* next = this;
+        
+        for(auto c: word) {
+            if(next->children[c] == nullptr)    return false;
+            
+            next = next->children[c];
         }
-        return cur->tail;
+        return next->is_tail;
     }
     
-    /** Returns if there is any word in the trie that starts with the given prefix. */
     bool startsWith(string prefix) {
-        Trie* cur = this;
-        for(int i=0;i<prefix.size();i++) {
-            if(cur->child[prefix[i]]==nullptr)    return false;
-            cur = cur->child[prefix[i]];
+        Trie* next = this;
+        
+        for(auto c: prefix) {
+            if(next->children[c] == nullptr)    return false;
+            
+            next = next->children[c];
         }
-        return cur->num_of_tail;
+        return true;
     }
 };
