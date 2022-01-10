07745490fc7b155b8c4391ddd26adafb5d03e75a
Kinaan-Oh <81892038+Kinaan-Oh@users.noreply.github.com>
2022-01-11 00:33:03 +0900

    Update #55. Jump Game_sol3.cpp

--- Leetcode/#55. Jump Game_sol3.cpp
+++ Leetcode/#55. Jump Game_sol3.cpp
@@ -1,4 +1,4 @@
-// Greedy, Time Complexity: O(n), Space Complexity: O(n^2) (n: nums.size)
+// Greedy, Time Complexity: O(n), Space Complexity: O(1) (n: nums.size)
 // sol2의 논리를 따름.
 // 다만, i에서 목적지에 도달가능한 j(i<j)는 목적지에 도달가능한 집합 중 최소값만 있어도 된다. 따라서, 목적지에 도달 가능한 최소 index 를 유지하여 문제를 해결할 수 있다.
 // 따라서, 목적지에 도달가능한 최소 index를 지속적으로 update 한다는 점에서 Greedy한 접근이라 볼 수 있다.
