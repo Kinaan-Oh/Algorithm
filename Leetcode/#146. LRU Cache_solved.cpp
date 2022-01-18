c4aea109a7c7aa55b77d483e1dae7d4fbec4e53b
Kinaan-Oh <81892038+Kinaan-Oh@users.noreply.github.com>
2022-01-18 22:07:29 +0900

    Update #146. LRU Cache_solved.cpp

--- Leetcode/#146. LRU Cache_solved.cpp
+++ Leetcode/#146. LRU Cache_solved.cpp
@@ -3,45 +3,46 @@
 // list는 그런 문제점이 있지 않을까하는 것이었다. 조사한 결과 list의 삽입/삭제시 resize가 일어나지 않으므로 다른 원소에 대한 iterator는 보존된다. 따라서, iterator를 포인터처럼 활용할 수 있게된다.
 // 결과적으로 해시테이블과 리스트(doubly linked list) 자료구조를 이용하여 get과 put의 Time Complexity를 O(1)로 구현할 수 있었다.
 
+// refactor: 1/18. pair를 이용하여 단일 hash 사용.
+
 class LRUCache {
 private:
-    unordered_map<int,int>  cache;
     int capacity;
-    list<int> order;
-    unordered_map<int,list<int>::iterator> pos_in_order;
+    unordered_map<int, list<pair<int,int>>::iterator>  pos;
+    list<pair<int,int>>   list; // (key:value)
     
 public:
-    LRUCache(int capacity) {
-        this->capacity = capacity;
+    LRUCache(int input) {
+        capacity = input;
     }
     
-    int get(int key) {
-        if(cache.find(key)==cache.end())   return -1;
+    int get(int key) { 
+        if(pos.find(key) == pos.end())  return -1;
         
-        update_order(key);
-        return cache[key];
+        update_pos(key);
+        return pos[key]->second;
     }
     
     void put(int key, int value) {
-        if(cache.size()==capacity && cache.find(key)==cache.end()) {
-            evict();
+        if(pos.find(key) != pos.end()) {
+            update_pos(key);
+            pos[key]->second = value;
+            return;
         }
         
-        update_order(key);
-        cache[key] = value;
+        if(list.size() == capacity)    evict();
+        list.push_back({key, value});
+        pos[key] = --list.end();
+    }
+    
+    void update_pos(int key) {
+        list.push_back(*pos[key]);
+        list.erase(pos[key]);
+        pos[key] = --list.end();
     }
     
     void evict() {
-        cache.erase(order.front());
-        order.pop_front();
-    }
-    
-    void update_order(int key) {
-        if(cache.find(key)!=cache.end()) {
-            order.erase(pos_in_order[key]);
-        }
-        
-        order.push_back(key);
-        pos_in_order[key] = --order.end();
+        pos.erase(list.front().first);
+        list.pop_front();
     }
 };
