59bc8ec26aff512a3d903dd117ec6825bf1c4fe2
Kinaan-Oh <81892038+Kinaan-Oh@users.noreply.github.com>
2022-02-07 22:11:38 +0900

    Update #42. Trapping Rain Water_sol5.cpp

--- Leetcode/#42. Trapping Rain Water_sol5.cpp
+++ Leetcode/#42. Trapping Rain Water_sol5.cpp
@@ -1,26 +1,26 @@
 // "Two-Pointer", Time Complexity: O(n), Space Complexity: O(1).
 // 2-Pass인 sol1을 투 포인터 기법을 이용하여 1-Pass로 개선하였다.
 
+// 2022/02/07 ReDo : DP sol을 Two-Pointer 기법을 이용해 1-Pass, Space Complexity O(1) 로 개선.
+
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int ans = 0;
-        int left_max=0, right_max=0;
-        int left=0, right=height.size()-1;
+        int answer = 0;
+        int left = 0;
+        int right = height.size()-1;
+        int maxLeftHeight = height.front();
+        int maxRightHeight = height.back();
         
         while(left<right) {
-            left_max = max(left_max, height[left]);
-            right_max = max(right_max, height[right]);
-            
-            if(left_max<right_max) {
-                ans += (left_max - height[left]);
-                left++;
-            }
-            else {
-                ans += (right_max - height[right]);
-                right--;
+            if(maxLeftHeight<maxRightHeight) {
+                answer += (maxLeftHeight-height[left++]);
+                maxLeftHeight = max(maxLeftHeight, height[left]);
+            } else {
+                answer += (maxRightHeight-height[right--]);
+                maxRightHeight = max(maxRightHeight, height[right]);
             }
         }
-        return ans;
+        return answer;
     }
 };
