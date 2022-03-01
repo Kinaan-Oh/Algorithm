c5e9046d87eebb8ff17d830598e2ba90af8f271d
Kinaan-Oh <81892038+Kinaan-Oh@users.noreply.github.com>
2022-03-01 21:58:17 +0900

    Create #219. Contains Duplicate II_sol2.cpp

---
+++ Leetcode/#219. Contains Duplicate II_sol2.cpp
@@ -0,0 +1,17 @@
+// Sliding Window, Time Complexity: O(nlgk), Space Complexity: O(k) (n: nums.size)
+// Kakao에 출제된 문제
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        set<int>  bst;
+        
+        for(int i=0;i<nums.size();i++) {
+            if(bst.find(nums[i]) != bst.end())  return true;
+            
+            if(i>=k)    bst.erase(bst.find(nums[i-k]));
+            bst.insert(nums[i]);
+        }
+        return false;
+    }
+};
