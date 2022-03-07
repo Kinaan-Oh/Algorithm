abd95f140cbf497c08757581c9e4120c2019fd7d
Kinaan-Oh <81892038+Kinaan-Oh@users.noreply.github.com>
2022-03-07 20:00:32 +0900

    Update #1460. Make Two Arrays Equal by Reversing Sub-arrays.cpp

--- Leetcode/#1460. Make Two Arrays Equal by Reversing Sub-arrays.cpp
+++ Leetcode/#1460. Make Two Arrays Equal by Reversing Sub-arrays.cpp
@@ -1,6 +1,6 @@
 // Sort, Time Complexity: O(nlgn), Space Complexity: O(1) (n: target.size)
-// 논리: 1) 인접한 두 원소를 reverse 하는 행위는 swap과 동일.
-//      2) 따라서, reverse를 통해 정렬가능(bubble sort는 swap을 이용한 정렬방법) 
+// 논리: 1) 2회의 reverse로 두 원소의 swap 표현가능.
+//      2) 따라서, reverse를 통해 정렬가능(ex: bubble sort는 swap을 이용한 정렬방법)함이 보장됨.
 //      3) 두 배열을 정렬 후, 두 배열의 원소가 순차적으로 동일하다면 true.
 
 class Solution {
