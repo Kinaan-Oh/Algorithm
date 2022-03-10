69f4ca02ee00eae8ec51d735bfc52ebc38dbd992
Kinaan-Oh <81892038+Kinaan-Oh@users.noreply.github.com>
2022-03-10 21:57:07 +0900

    Update #496. Next Greater Element I_sol2.cpp

--- Leetcode/#496. Next Greater Element I_sol2.cpp
+++ Leetcode/#496. Next Greater Element I_sol2.cpp
@@ -1,6 +1,6 @@
 // Stack, Time Complexity: O(n), Space Complexity: O(n) (n: nums2.size, nums1 is subset of nums2)
 // 논리: 1) num2 각 elem에 대하여 next greater elem을 탐색 및 memoize.
-//      1-1) typical한 monotone increasing stack 기법 적용. -> stack의 elem은 next greater elem을 만나면 pop.
+//      1-1) typical한 monotone decreasing stack 기법 적용. -> stack의 elem은 next greater elem을 만나면 pop.
 //      2) nums1에 대해 memoize된 next greater을 순서대로 answer에 추가.
 
 class Solution {
