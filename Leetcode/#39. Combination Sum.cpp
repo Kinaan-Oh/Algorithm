a44d4fc36787cadad0d787e44bb6271056058b8c
Kinaan-Oh <81892038+Kinaan-Oh@users.noreply.github.com>
2022-03-12 20:20:42 +0900

    Update #39. Combination Sum.cpp

--- Leetcode/#39. Combination Sum.cpp
+++ Leetcode/#39. Combination Sum.cpp
@@ -26,3 +26,31 @@ public:
         }
     }
 };
+
+// ReDo 2022/03/12 Combination/Backtrack, Time Complexity: O(nCk), Space Complexity: O(k)
+// 중복선택이 가능함을 반영하는 것이 관건.
+
+class Solution {
+private:
+    vector<vector<int>> answer;
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        dfs(candidates, {}, 0, 0, target);
+        return answer;
+    }
+    
+    void dfs(vector<int>& candidates, vector<int> prev, int start, int curSum, int target) {
+        if(curSum==target) {
+            answer.push_back(prev);
+            return;
+        } else if(curSum>target) {
+            return;
+        }
+        
+        prev.push_back(0);
+        for(int i=start;i<candidates.size();i++) {
+            prev.back() = candidates[i];
+            dfs(candidates, prev, i, curSum+candidates[i], target);
+        }
+    }
+};
