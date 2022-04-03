821c49315b0540a06c13a10d69455c12cd094db5
Kinaan-Oh <81892038+Kinaan-Oh@users.noreply.github.com>
2022-04-03 17:08:56 +0900

    Update #167. Two Sum II - Input array is sorted.cpp

--- Leetcode/#167. Two Sum II - Input array is sorted.cpp
+++ Leetcode/#167. Two Sum II - Input array is sorted.cpp
@@ -13,3 +13,27 @@ public:
         return {left+1, right+1};
     }
 };
+
+
+// 2022/04/03 
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int p1 = 0;
+        int p2 = numbers.size()-1;
+        
+        while(p1<p2) {
+            int sum = numbers[p1] + numbers[p2];
+            
+            if(sum<target) {
+                p1 += 1;
+            } else if(sum>target) {
+                p2 -= 1;
+            } else {
+                return {p1+1, p2+1};
+            }
+        }
+        return {-1,-1};
+    }
+};
